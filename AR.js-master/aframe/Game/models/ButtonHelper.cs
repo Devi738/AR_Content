b340d64eb29b15781b659f58868dfd126e35091e
Devi738 <67795463+Devi738@users.noreply.github.com>
2021-08-31 22:11:40 +0530

    cs435
    cs435

--- AR.js-master/aframe/Game/models/ButtonHelper.cs
+++ AR.js-master/aframe/Game/models/ButtonHelper.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 public class ButtonHelper : MonoBehaviour
 {
     //Literally just need to be able to keep track of a URL lol. This almost feels unnecessary. 
-    public string URL = "https://www.google.co.in/maps/place/Paradise+Cross+Roads,+Sandhu+Apartment,+Kalasiguda,+Secunderabad,+Telangana+500003/@17.4432902,78.4852776,17z/data=!3m1!4b1!4m5!3m4!1s0x3bcb9a11cf746ec9:0x3c347de47a12e553!8m2!3d17.4432902!4d78.4874663";
+    public string URL = "https://www.google.com/";
 
 }
